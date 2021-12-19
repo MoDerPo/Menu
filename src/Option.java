a066d1714c9a1e9c6664fca8dfb5501f0686c757
MoDerPo <69361959+MoDerPo@users.noreply.github.com>
2021-12-19 21:03:49 +0100

    Made constructors

--- src/Option.java
+++ src/Option.java
@@ -4,4 +4,14 @@ public class Option {
     public String label;
     public String shortcut;
     public String description;
+
+    public Option(String label, String shortcut, String description) {
+        this.label = label;
+        this.shortcut = shortcut;
+        this.description = description;
+    }
+
+    public Option(String label) {
+        this(label, genShortcut(label), label);
+    }
 }
